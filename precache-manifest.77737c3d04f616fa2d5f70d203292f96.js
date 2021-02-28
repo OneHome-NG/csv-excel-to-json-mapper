ebdff4dafaaf665170a9ed066080d950eaa11a56
LogicalOgbonna <arinzeogbo@gmail.com>
2021-02-28 19:02:50 +0100

    Updates

---
+++ precache-manifest.77737c3d04f616fa2d5f70d203292f96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16994f527ac5c4004bf7393208c4c4a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07e7b2587c9e6788730c",
+    "url": "./static/css/2.4a00cb5b.chunk.css"
+  },
+  {
+    "revision": "7baa4fa2698b85550b5d",
+    "url": "./static/css/main.c41d24d0.chunk.css"
+  },
+  {
+    "revision": "07e7b2587c9e6788730c",
+    "url": "./static/js/2.41d94bb6.chunk.js"
+  },
+  {
+    "revision": "e1ab716f4bfe300a2bc9120259febf5b",
+    "url": "./static/js/2.41d94bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7baa4fa2698b85550b5d",
+    "url": "./static/js/main.0e594141.chunk.js"
+  },
+  {
+    "revision": "a6b2560807bac7dd6109e9a007c7bea5",
+    "url": "./static/js/main.0e594141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c74d084d3686141904",
+    "url": "./static/js/runtime-main.bbe5e90b.js"
+  }
+]);
